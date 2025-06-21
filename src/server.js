1c7326f7e2a39ff72e2b7e1638390ad8d593ec8e
iamBablu <bablupamdey45671@gmail.com>
2025-06-21 18:08:48 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookiePasser from 'cookie-parser'
+
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json());
+app.use(cookiePasser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port  ${PORT}`);
+    connectDB();
+});
